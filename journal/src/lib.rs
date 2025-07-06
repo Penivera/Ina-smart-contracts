90c227c60464ec2a2be73953bbc28299ffe8f0f8
Peniel Ben <penielben40@gmail.com>
2025-07-06 22:50:06 +0000

    Updated journal to mint nft from a partcular entry and to track those nfts also added additonal checks to ensure right permissions

--- journal/src/lib.rs
+++ journal/src/lib.rs
@@ -1,44 +1,159 @@
-use std::collections::HashMap;
-use near_sdk::{borsh::{self, BorshDeserialize, BorshSerialize}, env, log, near_bindgen, AccountId, PanicOnDefault};
-use serde::{Serialize,Deserialize};
 mod types;
-use types as journal_types;
-use journal_types::{JournalEntry, Tag};
+use near_sdk::{env,
+    log,
+    AccountId,
+    PanicOnDefault,
+    near_bindgen,
+    store::{LazyOption},
+    collections::{UnorderedSet},
+    borsh::{self, BorshDeserialize, BorshSerialize}
+};
+use crate::types::{JournalEntry, Tag,HasNew};
+use near_contract_standards::non_fungible_token::{
+    metadata::{ NFTContractMetadata, TokenMetadata},
+    NonFungibleToken,  
+};
+use std::collections::HashMap;
 
 #[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault,Serialize,Deserialize)]
-pub struct JournalingVault {
-    entries: HashMap<AccountId, JournalEntry>,
+#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault,)]
+pub struct JournalingContract{
+    pub tokens: NonFungibleToken,
+    pub metadata: LazyOption<NFTContractMetadata>,
+    pub minters: UnorderedSet<AccountId>,
+    pub journal_entries: HashMap<String, JournalEntry>,
+    pub next_entry_id: u64, // Optional: if you want to track entries by ID
 }
+
 #[near_bindgen]
-impl JournalingVault {
+impl JournalingContract {
     #[init]
-    pub fn new() -> Self {
+    #[private]
+    pub fn initiate(media_url:String) -> Self {
+        let owner_id: AccountId = env::signer_account_id();
         Self {
-            entries: HashMap::new(),
+            journal_entries: HashMap::new(),
+            tokens: NonFungibleToken::new(
+                b"nft".to_vec(),owner_id.clone(),
+                Some(b"nft_approvals".to_vec()),
+                Some(b"nft_enumeration".to_vec()),
+                Some(b"nft_metadata".to_vec()),
+            ),
+            metadata: LazyOption::new(b"metadata".to_vec(), Some(NFTContractMetadata::new(media_url.clone()))),
+            minters: UnorderedSet::new(b"minters".to_vec()),
+            next_entry_id: 0,
         }
     }
 
     pub fn add_journal_entry(&mut self, content: String, tags: Vec<Tag>, is_private: bool) {
         let user: AccountId = env::predecessor_account_id();
+        assert!(content.len() > 0, "Entry content cannot be empty");
         let entry: JournalEntry = JournalEntry {
             user: String::from(user.clone()),
             content,
             tags,
             is_private,
+            timestamp: env::block_timestamp()/1e9 as u64,
+            entry_id: self.next_entry_id,
+            minted_nft: false, // Initially set to false, can be updated later
         };
         log!("Adding journal entry for user: {}, Private {:?}", user, entry.is_private);
-        self.entries.insert(user, entry);
+        self.journal_entries.insert(user.to_string(), entry);
     }
 
     pub fn get_user_entries(&self, user:AccountId ) -> Option<&JournalEntry> {
-        self.entries.get(&user)
+        self.journal_entries.get(&user.to_string())
     }
 
     pub fn get_public_entries(&self) -> Vec<&JournalEntry> {
-        self.entries
-            .values()
-            .filter(|entry| !entry.is_private)
-            .collect()
+        self.journal_entries.values().filter(|entry: &&JournalEntry| !entry.is_private).collect()
     }
+
+    pub fn get_private_entries(&self, user: AccountId) -> Vec<&JournalEntry> {
+        self.journal_entries.values().filter(|entry: &&JournalEntry| entry.is_private && entry.user == user).collect()
+    }
+
+    #[private]
+    pub fn get_all_entries(&self) -> Vec<&JournalEntry> {
+        self.journal_entries.values().collect()
+    }
+
+    #[payable]
+    pub fn mint_nft_for_entry(&mut self, entry_id: u64,media_url:String) {
+        let caller_id: AccountId = env::signer_account_id();
+        log!("Minting NFT for entry ID: {}, Caller: {}", entry_id, caller_id);
+        
+        let entry: &JournalEntry = self.journal_entries.get(&caller_id.to_string()).expect("Entry not found for caller");
+        assert!(!entry.minted_nft, "This entry has already been minted as an NFT");
+        assert!(entry.user == caller_id, "You can only mint your own entries");
+        //assert!(entry.is_private == false, "You cannot mint a private entry as an NFT");
+        assert!(media_url.len() > 0, "Media URL cannot be empty");
+
+
+        let token_metadata = TokenMetadata {
+            title: Some(format!("{:?} Journal Entry ", entry.tags)),
+            description: Some(entry.content.clone()),
+            media: Some(media_url.clone()),
+            copies: Some(1),
+            issued_at: Some(env::block_timestamp().to_string()),
+            expires_at: None,
+            starts_at: None,
+            updated_at: None,
+            extra: None,
+            reference: None,
+            reference_hash: None,
+            media_hash: None,
+        };
+
+        self.tokens.internal_mint(
+            entry_id.to_string(),
+            caller_id.clone(),
+            Some(token_metadata),
+        );
+        log!("NFT minted successfully for entry ID: {}", entry_id);
+
+        // Update the journal entry to mark it as minted
+        let mut updated_entry = entry.clone();
+        updated_entry.minted_nft = true;
+        self.journal_entries.insert(caller_id.to_string(), updated_entry);
+    }
+
+    #[private]
+    pub fn nft_metadata(&self) -> NFTContractMetadata {
+        let metadata = self.metadata.clone();
+        metadata.unwrap_or_else(|| NFTContractMetadata::new("".to_string()))
+    }
+
+    #[private]
+    pub fn get_minters(&self) -> Vec<AccountId> {
+        self.minters.to_vec()
+    }
+
+    #[private]
+    pub fn get_token_metadata(&self, journal_id: String) -> Option<TokenMetadata> {
+         assert!(!journal_id.is_empty(), "Journal ID cannot be empty");
+
+        let entry = self.journal_entries.get(&journal_id).expect("Journal entry not found");
+
+        assert!(entry.minted_nft, "This journal entry has not been minted as an NFT");
+        self.tokens.token_metadata_by_id.as_ref()?.get(&journal_id)
+
+    }
+
+    #[private]
+    pub fn get_journal_by_token_id(&self, token_id: String) -> Option<&JournalEntry> {
+        self.journal_entries.get(&token_id)
+    }
+
+    #[private]
+    pub fn get_journal_by_id(&self, entry_id: u64) -> Option<&JournalEntry> {
+        log!("Fetching journal entry for Entry ID: {}", entry_id);
+        self.journal_entries.values().find(|entry| entry.entry_id == entry_id)
+    }
+
+    pub fn get_minted_entries(&self) -> Vec<&JournalEntry> {
+        let user: AccountId = env::predecessor_account_id();
+        self.journal_entries.values().filter(|entry| entry.minted_nft && entry.user == user).collect()
+    }
+
 }
--- journal/src/types.rs
+++ journal/src/types.rs
@@ -1,8 +1,11 @@
 use near_sdk::{
     serde::{Deserialize, Serialize},
     borsh::{self, BorshDeserialize, BorshSerialize},
+    
 };
-
+use near_contract_standards::non_fungible_token::{
+    metadata::{NFTContractMetadata}
+};
 use schemars::JsonSchema;
 
 #[derive(
@@ -29,4 +32,30 @@ pub struct JournalEntry {
     pub content: String,
     pub tags: Vec<Tag>,
     pub is_private: bool,
+    pub timestamp: u64, // Optional: to track when the entry was created
+    pub entry_id: u64, // Optional: if you want to track entries by ID
+    pub minted_nft:bool, // Optional: to track if the entry has been minted as an NFT
 }
+
+pub trait HasNew {
+    fn new(media_url: String) -> Self;
+}
+
+impl HasNew for NFTContractMetadata {
+    fn new(media_url: String) -> Self {
+        let extract_cid = |url: &str| url.split("/ipfs/").nth(1).unwrap_or("").to_string();
+        Self {
+            spec: "nft-2.0.0".to_string(),
+            name: "unchainher-nft".to_string(),
+            symbol: "UHER".to_string(),
+            icon: Some(media_url.clone()),
+            base_uri: Some(format!("ipfs://{}", extract_cid(&media_url))),
+            reference: None,
+            reference_hash: None,
+        }
+    }
+}
+
+
+
+
